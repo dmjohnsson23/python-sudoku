63918fd92051a60d3d0c76905d1d566c6a8c417a
Dominick Johnson <dmjohn235@gmail.com>
2021-04-17 13:40:08 -0600

    Add stubs for more complex algorithms

--- sudoku/algorithms.py
+++ sudoku/algorithms.py
@@ -275,3 +275,106 @@ def find_naked_multiples(*houses):
                             modified = True
         
         return modified
+
+
+def find_x_wing(puzzle):
+    """
+    An x-wing is when there are 4 cells which are the corners of a given rectangular area of the grid, 
+    such that every one of the four cell shares a row with exactly one other cell, and a column with 
+    exactly one other cell. In these cells, all have the same candidate value. If either in both rows
+    occupied by the cells, or in both columns occupied by the cells, those two cells are the only two 
+    cells which can contain that value, this is an x-wing.
+
+    If there is an x-wing, that value can be removed as a possibility from all other cells in the rows
+    and columns occupied by the x-wing. For example image x represented all the open cells which can
+    possibly contain a given value in the puzzle:
+
+    ╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗
+    ║   │   │   ║   │   │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │ x ║   │   │   ║ x │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │ x ║   │   │   ║   │   │ x ║
+    ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+    ║   │   │   ║   │ x │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │ x ║   │   │   ║ x │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │ x │   ║   │ x │   ║ x │   │   ║
+    ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+    ║   │   │   ║   │   │   ║ x │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │   ║   │   │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║ x │ x │ x ║   │   │   ║   │ x │ x ║
+    ╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝
+
+    The cells in row 2 columns 3 and 7, and row 5 columns 3 and 7, form an x-wing, as they are the only
+    two possibilities in their rows. Thus, x can be removed from all other cells in their columns as well,
+    as follows:
+
+    ╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗
+    ║   │   │   ║   │   │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │ x ║   │   │   ║ x │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │   ║   │   │   ║   │   │ x ║
+    ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+    ║   │   │   ║   │ x │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │ x ║   │   │   ║ x │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │ x │   ║   │ x │   ║   │   │   ║
+    ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
+    ║   │   │   ║   │   │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║   │   │   ║   │   │   ║   │   │   ║
+    ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
+    ║ x │ x │   ║   │   │   ║   │ x │ x ║
+    ╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝
+    """
+    return False # TODO
+
+
+def find_y_wing(puzzle):
+    """
+    A y-wing takes three cells, a "stem" and two "branches". Each branch shares either a row, column, or square 
+    with the stem. The branch is limited to exactly two possible values: a and b. The first branch cell is also 
+    limited to two remaining values: b and c. The second branch cell is limited to a and c.
+
+    The y-wing tells us that the value c must be in one of the two branch cells, regardless of the value of the stem 
+    cell. Thus, any cell which shares either a row, column, or square with *both* branch cells cannot be a c.
+
+    For example:
+
+    ╔═══╤════════╤═══╦═══╤════════╤═══╦
+    ║   │        │   ║   │        │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │ [a, b] │   ║   │ [b, c] │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │        │   ║   │        │   ║
+    ╠═══╪════════╪═══╬═══╪════════╪═══╬
+    ║   │        │   ║   │        │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │ [a, c] │   ║   │ [c, d] │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │        │   ║   │        │   ║
+    ╠═══╪════════╪═══╬═══╪════════╪═══╬
+
+    Becomes:
+
+    ╔═══╤════════╤═══╦═══╤════════╤═══╦
+    ║   │        │   ║   │        │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │ [a, b] │   ║   │ [b, c] │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │        │   ║   │        │   ║
+    ╠═══╪════════╪═══╬═══╪════════╪═══╬
+    ║   │        │   ║   │        │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │ [a, c] │   ║   │    d   │   ║
+    ╟───┼────────┼───╫───┼────────┼───╫
+    ║   │        │   ║   │        │   ║
+    ╠═══╪════════╪═══╬═══╪════════╪═══╬
+    """
+    return False # TODO
